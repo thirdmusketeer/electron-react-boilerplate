72a5619e646bc327c61e2d4eadf23203a9a73052
Aramis <justganesh12321@gmail.com>
2020-11-04 18:12:33 +0530

    Initial commit

---
+++ app/containers/Root.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { hot } from 'react-hot-loader/root';
+import Routes from '../Routes';
+
+const Root = () => (
+  <Router>
+    <Routes />
+  </Router>
+);
+
+export default hot(Root);
